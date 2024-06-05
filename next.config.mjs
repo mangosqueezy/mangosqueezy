c322253b8b16f21bb6627759f26adf59dd62f1d4
amitmirgal <amit.s.mirgal@gmail.com>
2024-06-05 13:14:03 -0400

    added privacy and terms pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
