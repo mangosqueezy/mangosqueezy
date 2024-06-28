05105f6dde8e66451cffb27e5d4ac52863b9546f
amitmirgal <amit.s.mirgal@gmail.com>
2024-06-28 12:18:39 -0400

    fix(mail-list): fix build error on mail-list component

--- components/aceternity-ui/glare-card.tsx
+++ components/aceternity-ui/glare-card.tsx
@@ -54,7 +54,6 @@ export const GlareCard = ({
 
   const updateStyles = () => {
     if (refElement.current) {
-      console.log(state.current);
       const { background, rotate, glare } = state.current;
       refElement.current?.style.setProperty("--m-x", `${glare.x}%`);
       refElement.current?.style.setProperty("--m-y", `${glare.y}%`);
