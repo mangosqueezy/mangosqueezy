aa8d032aac86fd7d9a08895146ac8c01ec9a2b26
amitmirgal <amit.s.mirgal@gmail.com>
2024-08-24 13:00:28 -0400

    fix(buy): type error

--- app/buy/[...slug]/actions.ts
+++ app/buy/[...slug]/actions.ts
@@ -10,49 +10,52 @@ const SVIX_API_KEY = process.env.SVIX_API_KEY;
 const svix = new Svix(SVIX_API_KEY!);
 
 export async function createOrderAction(formData: FormData) {
-  const email = formData.get("email") as string;
-  const business_id = formData.get("business_id") as string;
-  const product_id = formData.get("product_id") as string;
-  const affiliate_id = formData.get("affiliate_id") as string;
-  const amount = formData.get("amount") as string;
+	const email = formData.get("email") as string;
+	const business_id = formData.get("business_id") as string;
+	const product_id = formData.get("product_id") as string;
+	const affiliate_id = formData.get("affiliate_id") as string;
+	const amount = formData.get("amount") as string;
 
-  try {
-    await createOrder({
-      email,
-      business_id,
-      product_id,
-      affiliate_id,
-    });
+	try {
+		await createOrder({
+			email,
+			business_id,
+			product_id,
+			affiliate_id,
+		});
 
-    const information = await getAffiliateBusinessInfoById(business_id, parseInt(affiliate_id));
+		const information = await getAffiliateBusinessInfoById(
+			business_id,
+			Number.parseInt(affiliate_id),
+		);
 
-    await payoutTask.trigger({
-      commission: information?.business.commission,
-      amount,
-      business_wallet_address: information?.business.wallet_address,
-      affiliate_wallet_address: information?.affiliate.wallet_address,
-      business_email: information?.business.email,
-      affiliate_email: information?.affiliate.email,
-    });
+		await payoutTask.trigger({
+			commission: information?.business.commission as number,
+			amount,
+			business_wallet_address: information?.business.wallet_address as string,
+			affiliate_wallet_address: information?.affiliate.wallet_address as string,
+			business_email: information?.business.email as string,
+			affiliate_email: information?.affiliate.email as string,
+		});
 
-    await svix.message.create(information?.business.svix_consumer_app_id!, {
-      eventType: "invoice.paid",
-      eventId: "evt_Wqb1k73rXprtTm7Qdlr38G",
-      payload: {
-        type: "invoice.paid",
-        product_id,
-        status: "paid",
-        email,
-      },
-    });
-  } catch (err) {
-    console.error(err);
-  }
+		await svix.message.create(information?.business.svix_consumer_app_id!, {
+			eventType: "invoice.paid",
+			eventId: "evt_Wqb1k73rXprtTm7Qdlr38G",
+			payload: {
+				type: "invoice.paid",
+				product_id,
+				status: "paid",
+				email,
+			},
+		});
+	} catch (err) {
+		console.error(err);
+	}
 
-  return "success";
+	return "success";
 }
 
 export async function navigate(data: FormData) {
-  const url = data.get("url") as string;
-  redirect(url);
+	const url = data.get("url") as string;
+	redirect(url);
 }
