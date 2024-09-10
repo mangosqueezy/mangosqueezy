95366f179b52b8c8b640ae322c07821bc202dbea
amitmirgal <amit.s.mirgal@gmail.com>
2024-09-10 19:42:44 -0400

    chore(env): rotate and update sentry key

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,15 +5,15 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://db4752446b3c9dbe97724a14df22d420@o4507738883489792.ingest.us.sentry.io/4507738885521408",
+	dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+	enabled: process.env.NODE_ENV === "production",
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+	// Adjust this value in production, or use tracesSampler for greater control
+	tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+	// Setting this option to true will print useful information to the console while you're setting up Sentry.
+	debug: false,
 
-  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: process.env.NODE_ENV === 'development',
-  
+	// Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+	// spotlight: process.env.NODE_ENV === 'development',
 });
