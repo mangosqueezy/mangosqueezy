ecd3b7bb6713a8f143f0612308a799385f28771b
amitmirgal <amit.s.mirgal@gmail.com>
2024-09-15 11:37:09 -0400

    refactor(layout): update layout with metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,55 @@ const fontSans = FontSans({
 export const metadata: Metadata = {
 	title: "mangosqueezy affiliates tool",
 	description: "The open source affiliate tool.",
+	openGraph: {
+		title: "mangosqueezy | Affiliates marketing tool",
+		description:
+			"A tool that takes care of all your affiliate marketing needs for your business.",
+		url: "https://mangosqueezy.com",
+		siteName: "mangosqueezy | Affiliates marketing tool",
+		locale: "en_US",
+		type: "website",
+		images: [
+			{
+				url: "https://lkjqkobxmgqedqtidcws.supabase.co/storage/v1/object/public/mangosqueezy/opengraph-image.jpg",
+				width: 800,
+				height: 600,
+			},
+			{
+				url: "https://lkjqkobxmgqedqtidcws.supabase.co/storage/v1/object/public/mangosqueezy/opengraph-image.jpg",
+				width: 1800,
+				height: 1600,
+			},
+		],
+	},
+	twitter: {
+		title: "mangosqueezy | Affiliates marketing tool",
+		description:
+			"A tool that takes care of all your affiliate marketing needs for your business.",
+		images: [
+			{
+				url: "https://lkjqkobxmgqedqtidcws.supabase.co/storage/v1/object/public/mangosqueezy/twitter-image.jpg",
+				width: 800,
+				height: 600,
+			},
+			{
+				url: "https://lkjqkobxmgqedqtidcws.supabase.co/storage/v1/object/public/mangosqueezy/twitter-image.jpg",
+				width: 1800,
+				height: 1600,
+			},
+		],
+	},
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			"max-video-preview": -1,
+			"max-image-preview": "large",
+			"max-snippet": -1,
+		},
+	},
 };
 
 export default function RootLayout({
