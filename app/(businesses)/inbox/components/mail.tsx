09bf466160de26045c241eff1d568a715d21bf64
amitmirgal <amit.s.mirgal@gmail.com>
2024-09-17 09:31:07 -0400

    fix(inbox): undefined error on inbox page

--- app/(businesses)/inbox/components/mail.tsx
+++ app/(businesses)/inbox/components/mail.tsx
@@ -1,53 +1,57 @@
 "use client";
 
-import * as React from "react";
-import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { MailDisplay } from "@/app/(businesses)/inbox/components/mail-display";
 import { MailList } from "@/app/(businesses)/inbox/components/mail-list";
 import { useMail } from "@/app/(businesses)/inbox/use-mail";
+import {
+	ResizableHandle,
+	ResizablePanel,
+	ResizablePanelGroup,
+} from "@/components/ui/resizable";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import type { TMessages } from "./mail-display";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import type { Products } from "@prisma/client";
+import * as React from "react";
+import type { TMessages } from "./mail-display";
 
 interface MailProps {
-  mails: TMessages[];
-  products: Array<Products> | undefined;
-  businessId: string;
+	mails: TMessages[];
+	products: Array<Products> | undefined;
+	businessId: string;
 }
 
 export function Mail({ mails, products, businessId }: MailProps) {
-  const [mail, setMail] = useMail();
+	const [mail, setMail] = useMail();
 
-  React.useEffect(() => {
-    setMail({
-      selected: mails[0].id,
-    });
-  }, [mails, setMail]);
+	React.useEffect(() => {
+		setMail({
+			selected: mails[0]?.id || null,
+		});
+	}, [mails, setMail]);
 
-  return (
-    <TooltipProvider delayDuration={0}>
-      <ResizablePanelGroup
-        direction="horizontal"
-        onLayout={(sizes: number[]) => {
-          document.cookie = `react-resizable-panels:layout=${JSON.stringify(sizes)}`;
-        }}
-        className="h-full max-h-[860px] items-stretch"
-      >
-        <ResizablePanel defaultSize={265} minSize={20}>
-          <MailList items={mails} />
-        </ResizablePanel>
-        <ResizableHandle withHandle />
-        <ResizablePanel defaultSize={655}>
-          <ScrollArea className="h-full w-full">
-            <MailDisplay
-              mail={mails.find(item => item.id === mail.selected) || null}
-              products={products}
-              businessId={businessId}
-            />
-          </ScrollArea>
-        </ResizablePanel>
-      </ResizablePanelGroup>
-    </TooltipProvider>
-  );
+	return (
+		<TooltipProvider delayDuration={0}>
+			<ResizablePanelGroup
+				direction="horizontal"
+				onLayout={(sizes: number[]) => {
+					document.cookie = `react-resizable-panels:layout=${JSON.stringify(sizes)}`;
+				}}
+				className="h-full max-h-[860px] items-stretch"
+			>
+				<ResizablePanel defaultSize={265} minSize={20}>
+					<MailList items={mails} />
+				</ResizablePanel>
+				<ResizableHandle withHandle />
+				<ResizablePanel defaultSize={655}>
+					<ScrollArea className="h-full w-full">
+						<MailDisplay
+							mail={mails.find((item) => item.id === mail.selected) || null}
+							products={products}
+							businessId={businessId}
+						/>
+					</ScrollArea>
+				</ResizablePanel>
+			</ResizablePanelGroup>
+		</TooltipProvider>
+	);
 }
--- app/(businesses)/orders/page.tsx
+++ app/(businesses)/orders/page.tsx
@@ -1,58 +1,69 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
 } from "@/components/ui/table";
 import { createClient } from "@/lib/supabase/server";
 import { getUserById } from "@/models/business";
-import { Orders } from "@prisma/client";
+import type { Orders } from "@prisma/client";
 import Menu from "./components/menu";
 
 export default async function OrdersPage() {
-  const supabase = createClient();
-  const { data } = await supabase.auth.getUser();
-  const userId = data?.user?.id as string;
-  const user = await getUserById(userId);
+	const supabase = createClient();
+	const { data } = await supabase.auth.getUser();
+	const userId = data?.user?.id as string;
+	const user = await getUserById(userId);
 
-  return (
-    <div className="flex min-h-screen w-full flex-col">
-      <Card>
-        <CardHeader>
-          <CardTitle>Orders</CardTitle>
-          <CardDescription>Manage your products and view their sales performance.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Customer Name</TableHead>
-                <TableHead>Product Name</TableHead>
-                <TableHead>
-                  <span className="sr-only">Actions</span>
-                </TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {user &&
-                user?.orders.map((order: Orders, index: number) => (
-                  <TableRow key={`table-row-${index}`}>
-                    <TableCell className="font-medium">{order.customer_email}</TableCell>
-                    <TableCell>
-                      {user.products.find(product => product.id === order?.product_id)?.name}
-                    </TableCell>
-                    <TableCell>
-                      <Menu orderId={order.id} />
-                    </TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
-        </CardContent>
-      </Card>
-    </div>
-  );
+	return (
+		<div className="flex min-h-screen w-full flex-col">
+			<Card>
+				<CardHeader>
+					<CardTitle>Orders</CardTitle>
+					<CardDescription>Manage your orders.</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<Table>
+						<TableHeader>
+							<TableRow>
+								<TableHead>Customer Name</TableHead>
+								<TableHead>Product Name</TableHead>
+								<TableHead>
+									<span className="sr-only">Actions</span>
+								</TableHead>
+							</TableRow>
+						</TableHeader>
+						<TableBody>
+							{user?.orders.map((order: Orders) => (
+								<TableRow key={order?.id}>
+									<TableCell className="font-medium">
+										{order.customer_email}
+									</TableCell>
+									<TableCell>
+										{
+											user.products.find(
+												(product) => product.id === order?.product_id,
+											)?.name
+										}
+									</TableCell>
+									<TableCell>
+										<Menu orderId={order.id} />
+									</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</CardContent>
+			</Card>
+		</div>
+	);
 }
