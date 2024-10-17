f4c394d6a5a0e766f4205ed1d796dee88c41003a
amitmirgal <amit.s.mirgal@gmail.com>
2024-10-16 21:13:52 -0400

    refactor(reply): delete reply endpoint

--- app/api/reply/schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-// define a schema for the reply
-export const replySchema = z.object({
-  email: z.object({
-    affiliateLink: z.boolean(),
-    content: z.string(),
-  }),
-});
