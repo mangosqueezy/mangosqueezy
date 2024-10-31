58d2796ce13ee01df3e4da7b262a80f21eb69b2b
amitmirgal <amit.s.mirgal@gmail.com>
2024-10-31 16:58:04 -0400

    refactor(editor): update tiptap editor css style

--- components/mango-ui/editor/extentions/bubble-menu/index.tsx
+++ components/mango-ui/editor/extentions/bubble-menu/index.tsx
@@ -27,7 +27,7 @@ export function BubbleMenu({
 		<TiptapBubbleMenu
 			editor={editor}
 			tippyOptions={tippyOptions}
-			className="bg-transparent"
+			className="bg-transparent ring-0 ring-white"
 		>
 			<div className="flex w-fit max-w-[90vw] overflow-hidden rounded-full border border-border bg-orange-50 text-gray-600 ring-1 ring-orange-600 text-mono font-regular">
 				{showAI ? (
