45c3d25c34f4174f4ea3cb6abfef400e2bb298e1
amitmirgal <amit.s.mirgal@gmail.com>
2024-11-03 14:46:18 -0500

    fix(tailwind config): fix build error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,7 @@ export default {
 			sans: ["var(--font-sans)", ...fontFamily.sans],
 		},
 		container: {
-			center: "true",
+			center: true,
 			padding: "2rem",
 			screens: {
 				"2xl": "1400px",
