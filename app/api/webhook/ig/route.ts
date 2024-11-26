029e060a9205625d543ed75f3826d6d117b358e5
amitmirgal <amit.s.mirgal@gmail.com>
2024-11-26 12:45:10 -0500

    refactor(ig_scope_identifier): update the ig_scope_identifier schema

--- app/api/webhook/ig/route.ts
+++ app/api/webhook/ig/route.ts
@@ -1,8 +1,4 @@
 import { isIgAssistEnabled } from "@/config/flags";
-import { decryptIgAccessToken } from "@/lib/utils";
-import { isAccessTokenExpiring } from "@/lib/utils";
-import { getIgAccessToken } from "@/models/ig_refresh_token";
-import { createIgScopeIdentifier } from "@/models/ig_scope_id";
 
 export async function GET(request: Request) {
 	const { searchParams } = new URL(request.url);
@@ -41,53 +37,6 @@ export async function POST(request: Request) {
 	if (messaging && messaging?.length > 0) {
 		for (const message of messaging) {
 			console.log("/api/webhook/ig/ POST => ", { message, appUsersIgUserId });
-
-			if (message.recipient.id !== IG_BUSINESS_ID) {
-				let INSTAGRAM_ACCESS_TOKEN = "";
-
-				const igAccessToken = await getIgAccessToken();
-				const isAccessTokenExpiringFlag = isAccessTokenExpiring(
-					igAccessToken?.expires_at as string,
-				);
-
-				if (isAccessTokenExpiringFlag) {
-					const refreshTokenResponse = await fetch(
-						"https://www.mangosqueezy.com/api/instagram/refresh_token",
-						{
-							method: "POST",
-							body: JSON.stringify({ access_token: igAccessToken?.token }),
-						},
-					);
-					const refreshTokenResult = await refreshTokenResponse.json();
-					const decryptedAccessToken = await decryptIgAccessToken(
-						refreshTokenResult?.encryptedHexString,
-						refreshTokenResult?.ivHexString,
-					);
-					INSTAGRAM_ACCESS_TOKEN = decryptedAccessToken;
-				} else {
-					const decryptedAccessToken = await decryptIgAccessToken(
-						igAccessToken?.token as string,
-						igAccessToken?.encryption_iv as string,
-					);
-					INSTAGRAM_ACCESS_TOKEN = decryptedAccessToken;
-				}
-
-				const recipientId = message.recipient.id;
-
-				const response = await fetch(
-					`https://graph.instagram.com/v21.0/${recipientId}?fields=name,username,profile_pic,follower_count,is_user_follow_business,is_business_follow_user&access_token=${INSTAGRAM_ACCESS_TOKEN}`,
-					{
-						method: "GET",
-					},
-				);
-
-				const result = await response.json();
-
-				await createIgScopeIdentifier({
-					recipient_id: recipientId,
-					ig_username: result?.username,
-				});
-			}
 
 			if (message.sender.id !== IG_BUSINESS_ID && igAssistEnabled) {
 				await fetch("https://mangosqueezy-hono-app.vercel.app/api/workflow", {
--- models/ig_scope_id.ts
+++ models/ig_scope_id.ts
@@ -1,21 +1,5 @@
 import prisma from "@/lib/prisma";
 
-export const createIgScopeIdentifier = async ({
-	recipient_id,
-	ig_username,
-}: { recipient_id: string; ig_username: string }) => {
-	return prisma.ig_scope_identifiers.upsert({
-		where: {
-			ig_scope_identifier: recipient_id,
-		},
-		update: {},
-		create: {
-			ig_scope_identifier: recipient_id,
-			ig_username,
-		},
-	});
-};
-
 export const getAvailableIgScopeIdentifier = async (take = 3) => {
 	return prisma.ig_scope_identifiers.findMany({
 		where: {
