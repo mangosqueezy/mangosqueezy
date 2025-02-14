d41ae2021cd8796f6985746ae9f64f3ac73565d9
AmitMirgal <amit.s.mirgal@gmail.com>
2025-02-14 11:02:41 -0500

    refactor(campaign action): add temporary email notification

--- app/(businesses)/campaigns/[slug]/actions.ts
+++ app/(businesses)/campaigns/[slug]/actions.ts
@@ -1,6 +1,8 @@
 "use server";
 
+import { EmailTemplate } from "@/components/mango-ui/email-template";
 import { createChatMessage } from "@/models/chat_message";
+import { Resend } from "resend";
 
 export async function createChatMessageAction(
 	pipeline_id: number,
@@ -12,6 +14,18 @@ export async function createChatMessageAction(
 		text: message,
 		sender: "amit@tapasom.com",
 		receiver: receiver,
+	});
+
+	// temporary email notification
+	const resend = new Resend(process.env.RESEND_API_KEY);
+	await resend.emails.send({
+		from: "mangosqueezy <amit@tapasom.com>",
+		to: ["amit@tapasom.com"],
+		subject: `New message to ${receiver} for pipeline ${pipeline_id}`,
+		react: EmailTemplate({
+			firstName: "there",
+			text: message,
+		}) as React.ReactElement,
 	});
 
 	return chatMessage;
