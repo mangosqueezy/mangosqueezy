69755c3797203aa50eccf37ea1d8a98464b0d8d4
AmitMirgal <amit.s.mirgal@gmail.com>
2025-05-16 16:10:36 -0400

    refactor(layout): update layout to use new AppSidebar and simplified header

--- app/(businesses)/layout.tsx
+++ app/(businesses)/layout.tsx
@@ -18,13 +18,17 @@ export default function DashboardLayout({
 		<SidebarProvider>
 			<AppSidebar />
 			<SidebarInset>
-				<header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+				<header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
 					<div className="flex items-center gap-2 px-4">
 						<SidebarTrigger className="-ml-1" />
-						<Separator orientation="vertical" className="mr-2 h-4" />
+						<Separator
+							orientation="vertical"
+							className="mx-2 data-[orientation=vertical]:h-4"
+						/>
+						<h1 className="text-base font-medium">mangosqueezy</h1>
 					</div>
 				</header>
-				<div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+				<div className="flex flex-1 flex-col gap-4 p-4 pt-0 my-3">
 					<Providers>{children}</Providers>
 				</div>
 			</SidebarInset>
