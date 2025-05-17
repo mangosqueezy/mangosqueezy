1222cd66133b4f831e8a22c2da396687327c319d
AmitMirgal <amit.s.mirgal@gmail.com>
2025-05-17 16:47:16 -0400

    fix(prisma): refactor Prisma client export to use default export

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,6 +2,8 @@ import { PrismaClient } from "@/prisma/app/generated/prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
