98e31c29db03991cfd2d4713a755eea3b1b701f2
AmitMirgal <amit.s.mirgal@gmail.com>
2025-05-21 23:04:00 -0400

    refactor(layout): remove proxy script and update analytics script configuration in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,12 +80,7 @@ export default function RootLayout({
 					)}
 				>
 					<script
-						src="/_proxy/squzy/script.js" // pointing to your reverse proxy
-						defer
-					/>
-					<script
 						src="https://pub-de4924ae66c74c129209cb58768d12fb.r2.dev/script.js"
-						data-api-host="/_proxy/squzy"
 						data-domains="{'refer':'go.squzy.link'}"
 						defer
 					/>
