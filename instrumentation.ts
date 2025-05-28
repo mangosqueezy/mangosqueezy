87e10911c1e4118e60781f752813a2c16736b7e6
AmitMirgal <amit.s.mirgal@gmail.com>
2025-05-28 15:36:37 -0400

    chore(otel): add vercel otel setup

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,4 +1,10 @@
+import { registerOTel } from "@vercel/otel";
+
 export async function register() {
+	registerOTel({
+		serviceName: "mangosqueezy",
+	});
+
 	if (process.env.NEXT_RUNTIME === "nodejs") {
 		await import("./sentry.server.config");
 	}
